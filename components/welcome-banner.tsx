82074fdb9cec81d49ae38e512ba3b659607aeeae
GabrielLogan-prog <gabriel@teiax.com>
2024-11-30 16:00:20 -0300

    alteracao na importacao de imagens

--- components/welcome-banner.tsx
+++ components/welcome-banner.tsx
@@ -1,11 +1,18 @@
+import Image from 'next/image'
+
 export function WelcomeBanner() {
-    return (
-      <div className="rounded-lg p-0 h-full w-full flex  justify-center relative overflow-hidden">
-    
-        <div className="flex flex-col items-center gap-4 z-10 w-[100%]">
-          <img src="/logo-large.png" alt="Genius" className="w-[100%]" />
-        </div>
+  return (
+    <div className="rounded-lg p-0 h-full w-full flex justify-center relative overflow-hidden">
+      <div className="flex flex-col items-center gap-4 z-10 w-[100%]">
+        <Image 
+          src="/logo-large.png" 
+          alt="Genius" 
+          width={800} 
+          height={200} 
+          className="w-full h-auto"
+        />
       </div>
-    )
-  }
-  
+    </div>
+  )
+}
+
