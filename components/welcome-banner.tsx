498d89cfa6174e676c0fba841f4c7bd7c9f1749b
GabrielLogan-prog <gabriel@teiax.com>
2024-12-02 18:43:57 -0300

    ajustes

--- components/welcome-banner.tsx
+++ components/welcome-banner.tsx
@@ -2,17 +2,14 @@ import Image from 'next/image'
 
 export function WelcomeBanner() {
   return (
-    <div className="rounded-lg p-0 h-full w-full flex justify-center relative overflow-hidden">
-      <div className="flex flex-col items-center gap-4 z-10 w-[100%]">
-        <Image 
-          src="/logo-large.png" 
-          alt="Genius" 
-          width={800} 
-          height={200} 
-          className="w-full h-auto"
-        />
-      </div>
+    <div className="rounded-lg h-full w-full relative overflow-hidden">
+      <Image 
+        src="/logo-large.png" 
+        alt="Genius" 
+        layout="fill"
+        objectFit="cover"
+        priority
+      />
     </div>
   )
 }
-
