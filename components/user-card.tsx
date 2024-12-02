7297d97e148d3e0b5040e2e716eb876fe0e270b1
GabrielLogan-prog <gabriel@teiax.com>
2024-12-02 13:45:41 -0300

    ajuste user card

--- components/user-card.tsx
+++ components/user-card.tsx
@@ -16,10 +16,10 @@ export function UserCard() {
           <div className="w-20 h-20 bg-gray-700 rounded-full flex items-center justify-center mb-3 text-lg font-bold">
             {userName.slice(0, 2).toUpperCase()}
           </div>
-          <h2 className="text-lg font-medium mb-3">Olá, {userName}!</h2>
+          <h2 className="text-lg font-medium mt-5">Olá, {userName}!</h2>
           <Button 
             variant="outline" 
-            className="w-[180px] h-[50px] bg-[#C4F400] border-[#88b337] text-[black] hover:bg-[#88b337] transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
+            className="w-[180px] h-[50px] mt-12 bg-[#C4F400] border-[#88b337] text-[black] hover:bg-[#C4F400] transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
             onClick={() => setIsModalOpen(true)}
           >
             Ver Perfil
